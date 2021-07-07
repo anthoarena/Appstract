fbf143c7cc98b24c2fd9c03577779049ff22493c
anthony <arena.antho@hotmail.com>
2021-07-07 18:48:00 +0200

    Add Controller implementation to create new contestant and getall contestants

--- AcmeContest/AcmeWebAPI/Controllers/ContestController.cs
+++ AcmeContest/AcmeWebAPI/Controllers/ContestController.cs
@@ -1,4 +1,5 @@
-﻿using AcmeInfrastructure.Interfaces;
+﻿using AcmeInfrastructure.DTO;
+using AcmeInfrastructure.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -18,6 +19,52 @@ namespace AcmeWebAPI.Controllers {
         public ContestController(ILogger<ContestController> logger, IContest repo) {
             _logger = logger;
             _repo = repo;
+        }
+
+
+
+        /// <summary>
+        /// Create new contestant 
+        /// </summary>
+        /// <param name="contestant"></param>
+        /// <returns>
+        /// Success or Error message
+        /// </returns>
+        [Route("contestant")]
+        [HttpPost]
+        public IActionResult NewContestant([FromBody] ContestantRequestDTO contestant) {
+            try {             
+                bool created = _repo.NewContestant(contestant);
+                if (!created) 
+                    return BadRequest("Contestant not created");
+                               
+                return Ok();
+            }
+            catch (Exception) {
+                return StatusCode(500);
+            }
+        }
+
+        /// <summary>
+        /// Return all contest's participants with their info 
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        [Route("contestant")]
+        [HttpGet]
+        public IActionResult GetAllContestants() {
+            try {
+                var response = _repo.AllContestant();
+
+                if (response == null)
+                    return BadRequest("An error has occured please try again");
+                         
+                return Ok(response);
+            }
+            catch (Exception) {
+                return StatusCode(500);
+            }
+         
         }
 
     }
