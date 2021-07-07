15ee62f40282a3e876d664e5515698fd6cf7dcbf
anthony <arena.antho@hotmail.com>
2021-07-07 14:41:46 +0200

    init unit test project

---
+++ AcmeContest/UnitTestAcme/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace UnitTestAcme {
+    public class Tests {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
