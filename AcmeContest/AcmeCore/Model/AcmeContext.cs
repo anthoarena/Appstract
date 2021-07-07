39bbbf7922d7aeaaff88546fd1edbfc81c8ef1e4
anthony <arena.antho@hotmail.com>
2021-07-07 14:46:31 +0200

    init console app project to create and export data

---
+++ AcmeContest/AcmeCore/Model/AcmeContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AcmeCore.Model {
+    public class AcmeContext :DbContext {
+    }
+}
